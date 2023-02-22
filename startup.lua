62420eb7d7fe2ace9ef4ebc5ad5dd36d019b7a2c
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-02-22 14:39:36 +0000

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,1 +1,9 @@
-
+io.write("Installing dependencies\n")
+shell.run("pastebin get JXJ1yCVL button.lua")
+os.loadAPI("button.lua")
+shell.run("clear")
+button.setMonitor(handler)
+startButton = button.create("Begin Installation")
+startButton.setPos(1,1)
+startButton.onClick(function() print("Installation Starting!") end)
+while true do button.await(startButton) end
