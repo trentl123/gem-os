68690d85955c2c242706b3da5b3bcf5c3a6d12dd
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-02-23 19:55:32 +0000

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -9,7 +9,14 @@ startButton.onClick(function() print("Installation Starting!") end)
 while true do button.await(startButton) end
 
 function initialAccountCreation()
-  io.write([[
-
-]])
+  io.write("Commencing initial account creation..\n")
+  os.sleep(2)
+  sys.run("clear")
+  io.write("Username: ")
+  io.read()
+  io.write("\nPassword: ")
+  io.read()
+  io.write("\nAlias (optional, leave blank): ")
+  io.read()
+  
 end
