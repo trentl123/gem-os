07daecfbdf3fa4253de43de79ab9ec16f5ea7f86
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-02-23 09:31:05 +0000

    Create accounts.lua

---
+++ accounts.lua
@@ -0,0 +1,18 @@
+AccountHandler = {}
+--[[
+  The AccountHandler class is designed to handle the management of user accounts on the OS.
+
+  Methods:
+    createAccount  - 
+    removeAccount  - 
+    setAccountPreferences  - 
+    getAccountPreferences  - 
+    loginAccount  - 
+    logoutAccount  - 
+    setAccountPrivileges  - 
+    getAccountPrivileges  - 
+  ]]
+function AccountHandler:createAccount()
+  -- Create a new account
+  
+end
