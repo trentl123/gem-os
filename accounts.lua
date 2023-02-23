c56b5ac67477f540a2ed56c833d477330bd4c0d3
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-02-23 15:25:54 +0000

    Update accounts.lua

--- accounts.lua
+++ accounts.lua
@@ -15,34 +15,73 @@ AccountHandler = {}
     createAccount (username) (password) (homePath) (priviliges) (preferences)  - Create a new user account on the OS.
     removeAccount (username)  - Remove the user account from the OS.
     setAccountPreferences (preference) (newValue)  - Set a preference or setting on the user's account.
-    getAccountPreferences  - 
+    getAccountPreferences (preference)  - Returns a preference of the user's account.
     loginAccount  - 
     logoutAccount  - 
     setAccountPrivileges  - 
     getAccountPrivileges  - 
 ]]
 function AccountHandler:createAccount(username, password, homePath, privileges, preferences)
+  if (self.instanceAlive == true) then return false end
+  t = setmetatable({} = {_index, AccountHandler})
+  t.instanceAlive = false
   local filepath = '/gemos/data/.accounts/' .. username
   accountFile = io.open(filepath, 'a')
-  accountFile.write(username .. "¬sep¬")
-  accountFile.write(password .. "¬sep¬")
-  accountFile.write(homePath .. "¬sep¬")
-  accountFile.write(privileges .. "¬sep¬")
-  accountFile.write(preferences["theme"] .. "¬sep¬")
-  accountFile.write(preferences["alias"] .. "¬sep¬")
+  accountFile.write("usr¬" .. username .. "¬sep¬")
+  accountFile.write("pss¬" .. password .. "¬sep¬")
+  accountFile.write("hmp¬" .. homePath .. "¬sep¬")
+  accountFile.write("prv¬" .. privileges .. "¬sep¬")
+  accountFile.write("thm¬" .. preferences["theme"] .. "¬sep¬")
+  accountFile.write("als¬" .. preferences["alias"] .. "¬sep¬")
   accountFile.write("\n")
   accountFile.close()
   fs.makeDir('/gemos/home/' .. homePath .. '/Documents/')
   fs.makeDir('/gemos/home/' .. homePath .. '/Downloads/')
   fs.makeDir('/gemos/home/' .. homePath .. '/Recycle Bin/')
+  return t
+end
+function AccountHandler:translatePreferenceCode(title)
+  if (self.instanceAlive == true) then return false end
+  local codes = {
+    ["username"] = "usr¬",
+    ["password"] = "pss¬",
+    ["homepath"] = "hmp¬",
+    ["privileges"] = "prv¬",
+    ["theme"] = "thm¬",
+    ["alias"] = "als¬",
+  }
+  return codes[title]
 end
 function AccountHandker:removeAccount(username)
+  if (self.instanceAlive == true) then return false end
   local filepath = '/gemos/data/.accounts/' .. username
   fs.delete(filepath())
   fs.delete('/gemos/home/' .. homePath)
+  self.instanceAlive = true
 end
 function AccountHandler:setAccountPreferences(preference, newValue)
+  if (self.instanceAlive == true) then return false end
   filepath = '/gemos/data/.accounts/ .. username
   local contents = fs.open(filepath, 'r')
   contents = splitBy(contents, '¬sep¬')
-  for 
+  finalString = ""
+  for _, eachValue in pairs(contents) do
+    if splitBy(eachValue, '¬')[0] == preference do
+      finalString = finalString .. newValue .. "¬sep¬"
+    else
+      finalString = finalString .. eachValue .. "¬sep¬"
+    end
+  end
+end
+function AccountHandler:getAccountPreferences(preference)
+  if (self.instanceAlive == true) then return false end
+  filepath = '/gemos/data/.accounts/ .. username
+  local contents = fs.open(filepath, 'r')
+  contents = splitBy(contents, '¬sep¬')
+  finalString = ""
+  for _, eachValue in pairs(contents) do
+    if splitBy(eachValue, '¬')[0] == preference do
+      return splitBy(eachValue, '¬')[1]
+    end
+  end
+end
