b4185e53416f57a7246e7b6864f115a210498ed1
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-02-23 09:58:41 +0000

    Update accounts.lua

--- accounts.lua
+++ accounts.lua
@@ -3,16 +3,26 @@ AccountHandler = {}
   The AccountHandler class is designed to handle the management of user accounts on the OS.
 
   Methods:
-    createAccount  - 
-    removeAccount  - 
-    setAccountPreferences  - 
+    createAccount (username) (password) (homePath) (priviliges) (preferences)  - Create a new user account on the OS.
+    removeAccount (username)  - Remove the user account from the OS.
+    setAccountPreferences (preference) (newValue)  - Set a preference or setting on the user's account.
     getAccountPreferences  - 
     loginAccount  - 
     logoutAccount  - 
     setAccountPrivileges  - 
     getAccountPrivileges  - 
-  ]]
-function AccountHandler:createAccount()
-  -- Create a new account
-  
+]]
+function AccountHandler:createAccount(username, password, homePath, privileges, preferences)
+  accountFile = io.open('/gemos/data/accounts', 'a')
+  accountFile.write(username .. "¬sep¬")
+  accountFile.write(password .. "¬sep¬")
+  accountFile.write(homePath .. "¬sep¬")
+  accountFile.write(privileges .. "¬sep¬")
+  accountFile.write(preferences["theme"] .. "¬sep¬")
+  accountFile.write(preferences["alias"] .. "¬sep¬")
+  accountFile.write("\n")
+  accountFile.close()
+  fs.makeDir('/gemos/home/' .. homePath .. '/Documents/')
+  fs.makeDir('/gemos/home/' .. homePath .. '/Downloads/')
+  fs.makeDir('/gemos/home/' .. homePath .. '/Recycle Bin/')
 end
