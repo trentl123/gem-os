f060bf138ab4c327c696596d70b78f96d4b4e4d9
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-02-24 10:14:56 +0000

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -7,16 +7,32 @@ startButton = button.create("Begin Installation")
 startButton.setPos(1,1)
 startButton.onClick(function() print("Installation Starting!") end)
 while true do button.await(startButton) end
-
 function initialAccountCreation()
   io.write("Commencing initial account creation..\n")
   os.sleep(2)
   sys.run("clear")
   io.write("Username: ")
-  io.read()
+  local username = io.read()
   io.write("\nPassword: ")
-  io.read()
+  local password = io.read()
   io.write("\nAlias (optional, leave blank): ")
-  io.read()
-  
+  local alias io.read()
+  io.write("\nPreparing for account creation..")
+  os.loadAPI("accounts.lua")
+  os.sleep(0.6)
+  io.write("\nPerforming account creation..")
+  AccountHandler:createAccount(username, password, '/gemos/home/' .. string.lower(username), 3, {["theme"] = "ruby", ["alias"] = alias})
+  os.sleep(3)
+  io.write("\nAccount created!")
+  os.sleep(1)
+  sys.run("clear)
 end
+
+io.write([[
+  
+               Gem OS   
+  
+         Just getting things
+          ready for you....
+  
+]])
