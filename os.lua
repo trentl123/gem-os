ca6e15e78ee2d73e0e249048c257dbcdce8fcd30
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-03-01 10:46:17 +0000

    Update os.lua

--- os.lua
+++ os.lua
@@ -24,30 +24,6 @@ function getTableLength(table)
   return count
 end
 
-Error = {}
---[[
-  The Error class will handle and keep track of the operating system's errors.
-  
-  Methods:
-    run (errcode)  - Creates a new error object.
-    parseCode (errcode)  - Converts the code into a string error message.
-  ]]
-function Error:new(errcode)
-  local t = setmetatable({}, { __index = Error})
-  t.code = errcode
-  return t
-end
-function Error:parseCode(errcode)
-  local errorCodes = {
-    [1] = "That file/directory doesn't exist or cannot be found.",
-    [2] = "That file/directory could not be created.",
-    [3] = "That file/directory cannot be removed.",
-    [4] = "You cannot modify essential operating system files.",
-    [5] = "Your OS installation has become corrupt! Please exit to shell, load the installation disk and type '/disk/repair.lua'.",
-  }
-  return errorCodes[errcode]
-end
-
 FileExplorer = {}
 --[[
   The FileExplorer class is designed to create a new instance of a file explorer inside the operating system.
