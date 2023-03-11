9a70977047eed22f7f58a830e04e5d43fc5542e3
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-03-11 17:12:15 +0000

    Update applications.lua

--- handlers/applications.lua
+++ handlers/applications.lua
@@ -1,5 +1,9 @@
 ApplicationHandler = {}
 
+function string.starts(String,Start)
+   return string.sub(String,1,string.len(Start))==Start
+end
+
 function ApplicationHandler:open(appname)
   local path = "/gemos/applications/" .. appname
   sys.execute(path .. "/main.lua")
@@ -11,4 +15,8 @@ function ApplicationHandler:prlist()
   for x, _ in pairs(applist) do
     io.write("\n" .. x)
   end
+end
+function ApplicationHandler:info(app)
+  local manifest = fs.open("/gemos/applications/" .. app .. "/app.manifest", "r")
+  manifest
 end
