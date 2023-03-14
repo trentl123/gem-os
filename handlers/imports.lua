777b697662caabae738e7d68a879472bc624cb63
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-03-14 09:14:03 +0000

    Update imports.lua

--- handlers/imports.lua
+++ handlers/imports.lua
@@ -1,3 +1,25 @@
-function importDependencies()
+import = {}
+function import:accounts()
   os.loadAPI("handlers/accounts.lua")
+end
+function import:applications()
+  os.loadAPI("handlers/applications.lua")
+end
+function import:colour()
+  os.loadAPI("handlers/colour.lua")
+end
+function import:errors()
+  os.loadAPI("handlers/errors.lua")
+end
+function import:fileexplorer()
+  os.loadAPI("handlers/fileexplorer.lua")
+end
+function import:notifications()
+  os.loadAPI("handlers/notifications.lua")
+end
+function import:themes()
+  os.loadAPI("handlers/themes.lua")
+end
+function import:memory()
+  os.loadAPI("handlers/memory.lua")
 end
