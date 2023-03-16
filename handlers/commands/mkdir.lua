a295d1e1ac61d372baf274c949c24270cc171eb7
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-03-16 14:52:42 +0000

    Update mkdir.lua

--- handlers/commands/mkdir.lua
+++ handlers/commands/mkdir.lua
@@ -1,7 +1,7 @@
 function runCommand(arguments, flags)
   if #arguments > 1 then
-    
+    fs.makedir('/gemos/' .. arguments[2])
   else
-    return false
+    return false -- Error return here
   end
 end
