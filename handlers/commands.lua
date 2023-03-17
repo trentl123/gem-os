4c08f57c857af85a3dbbe3822cba0a19a31cf7c4
trentl123 <75752033+trentl123@users.noreply.github.com>
2023-03-17 14:08:35 +0000

    Update commands.lua

--- handlers/commands.lua
+++ handlers/commands.lua
@@ -1,7 +1,21 @@
 commandInterpreter = {}
 
-function commandInterpreter:getArguments(cmd)
-  local arguments = {}
-  for x in cmd:gmatch("%S+") do table.insert(arguments, x)
-  return arguments
+function commandInterpreter:run(commandString)
+  local commandArgs = {}
+  local flags = {}
+  for word in commandString:gmatch("%S+") do
+      if word:sub(1, 1) == "-" then
+          table.insert(flags, word)
+      else
+          table.insert(commandArgs, word)
+      end
+  end
+  local commandFilePath = "/gemos/sys/commands/" .. commandArgs[1] .. ".lua"
+  local commandFile = io.open(commandFilePath, "r")
+  if commandFile == nil then
+      return false
+  end
+  commandFile:close()
+  local commandModule = require(commandArgs[1])
+  commandModule.runCommand(commandArgs, flags)
 end
